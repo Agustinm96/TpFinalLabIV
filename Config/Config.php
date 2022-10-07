21d0f26a40f379513bf10301def0c3c056daeb13
Agustin Alvarez <agustinmalvarezok@gmail.com>
2022-10-07 19:11:25 -0300

    Framework Arquitechture

---
+++ Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Config;
+
+define("ROOT", dirname(__DIR__) . "/");
+define("FRONT_ROOT", "/TpFinalLabIV");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/"); /*empty*/
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/"); /*empty*/
+define("IMG_PATH", VIEWS_PATH . "img/"); /*empty*/
+
+?>
